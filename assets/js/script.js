b0f2fe9874574b268df3d510bd50f03127113bda
oliinykroman <oliinykroman@gmail.com>
2024-03-07 23:58:13 +0200

    added new sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
 
+    $('.trusted-slider').slick({
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        // autoplay: true,
+        // autoplaySpeed: 2000,
+    });
+
 });
