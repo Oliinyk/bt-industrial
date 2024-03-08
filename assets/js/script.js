0764a525965714bc3e6bb0f16d968e7b2e5fcbf2
oliinykroman <oliinykroman@gmail.com>
2024-03-08 03:07:11 +0200

    added new sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,27 @@ $(document).ready(function () {
     $('.trusted-slider').slick({
         slidesToShow: 7,
         slidesToScroll: 1,
-        // autoplay: true,
-        // autoplaySpeed: 2000,
+        autoplay: true,
+        autoplaySpeed: 3000,
+    });
+
+    $('.custom-quote-slider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+    });
+    
+    $(".scroll-to-top").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, 2000);
+    });
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 500) {
+            $(".scroll-to-top").fadeIn(300);
+        }
+        else {
+            $(".scroll-to-top").fadeOut(300);
+        }
     });
 
 });
