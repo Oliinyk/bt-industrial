944b89efc80f6cb404b7a576df23946082814d00
oliinykroman <oliinykroman@gmail.com>
2024-03-09 23:46:05 +0200

    responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,6 +22,21 @@ $(document).ready(function () {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 5.5,
+                arrows: false,
+              }
+            },
+            {
+              breakpoint: 767,
+              settings: {
+                slidesToShow: 4.5,
+              }
+            }
+        ]
     });
 
     $('.custom-quote-slider').slick({
