0fa493437c33b7b239acb9a333ee88bac585db2b
oliinykroman <oliinykroman@gmail.com>
2024-03-11 23:10:34 +0200

    added new sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,25 +76,27 @@ $(document).ready(function () {
     });
 
     //industrial-slider1
-    $('.industrial-slider1').slick({
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        arrows: true,
-        dots: true,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 2,
-                }
-            },
-            {
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 1,
-                }
-            },
-        ]
+    $('.industrial-slider1').each(function() {
+        $( this ).slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: true,
+            dots: true,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 767,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                },
+            ]
+        });
     });
 
     $('.industrial-slider2').slick({
