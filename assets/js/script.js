ab7aab8f49e871f229f16a785b810876426e1ed1
oliinykroman <oliinykroman@gmail.com>
2024-03-11 21:23:26 +0200

    added new sections

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,6 +75,49 @@ $(document).ready(function () {
         ]
     });
 
+    //industrial-slider1
+    $('.industrial-slider1').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    });
+
+    $('.industrial-slider2').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+    });
+
     $('.custom-quote-slider').on('beforeChange', function(event, slick, currentSlide, nextSlide){
         console.log(nextSlide);
         $('.digit').removeClass('active');
